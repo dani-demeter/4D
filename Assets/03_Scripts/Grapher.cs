207cd1711e428c401d0a886ac9cee5f6ac30a996
flamegiraffe <flamegiraffes@gmail.com>
2018-03-06 13:28:26 -0800

    torus fully functional

--- Assets/03_Scripts/Grapher.cs
+++ Assets/03_Scripts/Grapher.cs
@@ -6,39 +6,47 @@ using UnityEngine;
 public class Grapher : MonoBehaviour{
 	public GameObject ULR;
 	public GameObject VLR;
+	public GameObject DLR;
+	public GameObject meshPrefab;
+	public GameObject pointPrefab;
 	[Space(20)]
-	[Range (10, 100)]
+	[Range (2, 100)]
 	public int Sres = 10;
-	[Range (10, 100)]
+	[Range (2, 100)]
 	public int Tres = 10;
 	[Space (20)]
-	public int maxS;
-	public int maxT;
-	public int minS;
-	public int minT;
+	public float maxS;
+	public float maxT;
+	public float minS;
+	public float minT;
 	[Space (20)]
-	public GameObject pointPrefab;
 
 	private List<LineRenderer> lrs = new List<LineRenderer>();
+	private List<MeshFilter> meshes = new List<MeshFilter>();
 	private GameObject[,] points;
 	private float scale=1f;
-	private float a = Mathf.PI/4;
+	public float a = 0f;
 	private float da = 0f;
+	private float e = 2.7182818f;
 	// private float a = 0f;
 	float fx (float s, float t){
 		// return (1.25f+0.5f*Mathf.Cos(s))*Mathf.Cos(t);
 		return Mathf.Cos(s);
+		// return s;
 	}
 	float fy (float s, float t){
 		// return (1.25f+0.5f*Mathf.Cos(s))*Mathf.Sin(t);
 		return Mathf.Cos(a)*Mathf.Sin(s)+Mathf.Sin(a)*Mathf.Sin(t);
+		// return Mathf.Cos(a)*s+Mathf.Sin(a)*Mathf.Exp(s)*Mathf.Cos(t);
 	}
 	float fu (float s, float t){
 		// return 0.5f*Mathf.Sin(s);
 		return Mathf.Cos(t);
+		// return Mathf.Exp(s)*Mathf.Sin(t);
 	}
 	float fv (float s, float t){
 		return -Mathf.Sin(a)*Mathf.Sin(s)+Mathf.Cos(a)*Mathf.Sin(t);
+		// return -Mathf.Sin(a)*s+Mathf.Cos(a)*Mathf.Exp(s)*Mathf.Cos(t);
 	}
 	public void Invert(){
 		Vector3 o = new Vector3(0,3,0);
@@ -67,6 +75,9 @@ public class Grapher : MonoBehaviour{
 				float x = (1/(Mathf.Sqrt(2)-fv(cS, cT)))*fx(cS, cT);
 				float y = (1/(Mathf.Sqrt(2)-fv(cS, cT)))*fy(cS, cT);
 				float z = (1/(Mathf.Sqrt(2)-fv(cS, cT)))*fu(cS, cT);
+				// float x = fx(cS, cT);
+				// float y = fy(cS, cT);
+				// float z = fu(cS, cT);
 
 				p.transform.localPosition = new Vector3 (x, y, z);
 			}
@@ -94,13 +105,34 @@ public class Grapher : MonoBehaviour{
 			}
 			lr.SetPositions(poss);
 		}
+		for(int i=0; i<Sres; i++){
+			LineRenderer lr = lrs[i+Tres+Sres];
+			lr.positionCount = Tres;
+			Vector3[] poss = new Vector3[lr.positionCount];
+			for(int j=0; j<Tres; j++){
+				poss[j] = points[(i+j)%Sres,j].transform.localPosition;
+			}
+			lr.SetPositions(poss);
+		}
+		// for(int j=0; j<Tres; j++){
+		// 	LineRenderer lr = lrs[j+Sres+Tres+Sres];
+		// 	lr.positionCount = Sres;
+		// 	Vector3[] poss = new Vector3[lr.positionCount];
+		// 	for(int i=0; i<Sres; i++){
+		// 		poss[i] = points[i,(j-i+Tres)%Tres].transform.localPosition;
+		// 	}
+		// 	lr.SetPositions(poss);
+		// }
 		// for(int i=0; i<Sres; i++){
 		// }
 	}
 	void Update(){
 		for(int i=0; i<lrs.Count; i++){
-			lrs[i].startWidth = scale*0.05f;
+			lrs[i].startWidth = scale*0.03f;
 		}
+		// for(int i=lrs.Count/2; i<lrs.Count; i++){
+		// 	lrs[i].startWidth = scale*0.01f;
+		// }
 	}
 	void FixedUpdate(){
 		if(da!=0){
@@ -122,6 +154,9 @@ public class Grapher : MonoBehaviour{
 				float x = (1/(Mathf.Sqrt(2)-fv(cS, cT)))*fx(cS, cT);
 				float y = (1/(Mathf.Sqrt(2)-fv(cS, cT)))*fy(cS, cT);
 				float z = (1/(Mathf.Sqrt(2)-fv(cS, cT)))*fu(cS, cT);
+				// float x = fx(cS, cT);
+				// float y = fy(cS, cT);
+				// float z = fu(cS, cT);
 
 				p.transform.position = new Vector3 (x, y, z);
 				p.transform.parent = transform;
@@ -129,6 +164,7 @@ public class Grapher : MonoBehaviour{
 			}
 		}
 		DrawLines();
+		CreateMeshes();
 		transform.RotateAround (Vector3.zero, new Vector3 (1, 0, 0), 90);
 	}
 
@@ -161,7 +197,79 @@ public class Grapher : MonoBehaviour{
 			lr.SetPositions(poss);
 			lrs.Add(lr);
 		}
+		for(int i=0; i<Sres; i++){
+			GameObject e = Instantiate(DLR) as GameObject;
+			e.transform.parent = transform;
+			LineRenderer lr = e.GetComponent<LineRenderer>();
+			lr.positionCount = Tres;
+			Vector3[] poss = new Vector3[lr.positionCount];
+			for(int j=0; j<Tres; j++){
+				poss[j] = points[(i+j)%Sres,j].transform.position;
+			}
+			lr.SetPositions(poss);
+			lrs.Add(lr);
+		}
+		// for(int j=0; j<Tres; j++){
+		// 	GameObject e = Instantiate(DLR) as GameObject;
+		// 	e.transform.parent = transform;
+		// 	LineRenderer lr = e.GetComponent<LineRenderer>();
+		// 	lr.positionCount = Sres;
+		// 	Vector3[] poss = new Vector3[lr.positionCount];
+		// 	for(int i=0; i<Sres; i++){
+		// 		poss[i] = points[i,(j-i+Tres)%Tres].transform.position;
+		// 	}
+		// 	lr.SetPositions(poss);
+		// 	lrs.Add(lr);
+		// }
+
 		// for(int i=0; i<Sres; i++){
 		// }
+	}
+	void CreateMeshes(){
+		// // GameObject m = Instantiate(meshPrefab) as GameObject;
+		// // Mesh mesh = new Mesh();
+		// // m.GetComponent<MeshFilter>().mesh = mesh;
+		// // Vector3[] vs = {new Vector3(0,0,0), new Vector3(0,1,0), new Vector3(1,0,0)};
+		// // mesh.vertices = vs;
+		// // int[] tri = new int[3];
+		// // m.transform.parent = transform;
+		// // tri[0] = 0;
+		// // tri[1] = 1;
+		// // tri[2] = 2;
+		// // mesh.triangles = tri;
+		// GameObject m = Instantiate(meshPrefab) as GameObject;
+		// Mesh mesh = new Mesh();
+		// m.GetComponent<MeshFilter>().mesh = mesh;
+		// Vector3[] vs = new Vector3[Sres*Tres];
+		// for(int i=0; i<Sres; i++){
+		// 	for(int j=0; j<Tres; j++){
+		// 		vs[i*Tres+j] = points[i, j].transform.position;
+		// 	}
+		// }
+		// mesh.vertices = vs;
+		// int[] tris = new int[Sres*Tres*2*3];
+		// // for(int i=0; i<Sres; i++){
+		// 	// for(int j=0; j<Tres; j++){
+		// int i=0;
+		// int j=0;
+		// 		int ind = i*Tres*6+j*6;
+		// 		tris[ind] = i*Tres+j;
+		// 		tris[ind+1] = (i+1)*Tres+j;
+		// 		tris[ind+2] = (i+1)*Tres+j+1;
+		// 		ind += 3;
+		// 		tris[ind] = i*Tres+j;
+		// 		tris[ind+1] = i*Tres+j+1;
+		// 		tris[ind+2] = (i+1)*Tres+j+1;
+		// 		// Debug.Log((i*Tres+j) + " " + ((i+1)*Tres+j)+ " " + ((i+1)*Tres+j+1));
+		// 	// }
+		// // }
+		// // Vector2[] uvs = new Vector2[vs.Length];
+    // // for (int i = 0; i < uvs.Length; i++)
+    // // {
+    // // 	uvs[i] = new Vector2(vs[i].x, vs[i].z);
+    // // }
+    // // mesh.uv = uvs;
+		// mesh.triangles = tris;
+		// m.transform.parent = transform;
 	}
 }
